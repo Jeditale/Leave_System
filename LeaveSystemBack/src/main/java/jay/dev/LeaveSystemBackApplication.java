19bf25402c24cf2403db5578294acd0024a49df5
Jeditale <69350223+Jeditale@users.noreply.github.com>
2025-02-10 15:44:30 +0700

    Finished docker

---
+++ LeaveSystemBack/src/main/java/jay/dev/LeaveSystemBackApplication.java
@@ -0,0 +1,13 @@
+package jay.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveSystemBackApplication.class, args);
+	}
+
+}
---
+++ LeaveSystemBack/src/test/java/jay/dev/LeaveSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package jay.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
